68eafe1ab14c0e60f1b8360a17b519a3b20f5c05
Alexander Novikov <mem-memov@yandex.ru>
2023-07-29 23:55:22 +0300

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,7 +10,12 @@ import collection.JavaConverters.*
   comPort.setComPortParameters(9600, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY)
 //  comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0)
   val isOpen = comPort.openPort()
-  val listener = new MessageListener
+
+  val process = (bits: Array[Boolean]) =>
+    bits.map(if _ then "1" else "0").map(print)
+    println()
+
+  val listener = MessageListener(process)
   comPort.addDataListener(listener)
 
   try
--- src/main/scala/net/mem_memov/fingers/MessageListener.scala
+++ src/main/scala/net/mem_memov/fingers/MessageListener.scala
@@ -2,7 +2,7 @@ package net.mem_memov.fingers
 
 import com.fazecast.jSerialComm.*
 
-class MessageListener extends SerialPortMessageListener:
+case class MessageListener(process: Array[Boolean] => Unit) extends SerialPortMessageListener:
 
   override def getMessageDelimiter: Array[Byte] =
 
@@ -18,15 +18,14 @@ class MessageListener extends SerialPortMessageListener:
 
   override def serialEvent(event: SerialPortEvent): Unit =
 
-    val bytes = event
+    val bits = event
       .getReceivedData
       .dropRight(getMessageDelimiter.length) // drop line delimiter
       .map(_.toChar)
       .mkString("")
       .split(" ")
-      .map(_.toInt)
-      .foreach { byteInt =>
-        val byteBits = Vector(
+      .map(_.toInt).flatMap { byteInt =>
+        Array(
           ((byteInt >> 0) & 1) == 1,
           ((byteInt >> 1) & 1) == 1,
           ((byteInt >> 2) & 1) == 1,
@@ -36,15 +35,6 @@ class MessageListener extends SerialPortMessageListener:
           ((byteInt >> 6) & 1) == 1,
           ((byteInt >> 7) & 1) == 1,
         ).reverse
+      }.take(24)
 
-        val s = byteBits.map(bit => if bit then "1" else "0").mkString("")
-        print(s)
-        print(" ")
-      }
-
-    println()
-
-  def toBinaryString(byte: Byte): String =
-
-    val zeroBased = byte.toInt + Byte.MaxValue.toInt + 1
-    String.format("%8s", Integer.toBinaryString(zeroBased & 0xFF)).replace(' ', '0')
+    process(bits)
